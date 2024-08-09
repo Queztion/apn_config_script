1d186c6c7de432ee112b5d9cc2fe24b4ba4e8366
Richie Roy Jayme <58279554+Queztion@users.noreply.github.com>
2024-08-09 18:25:09 +0900

    Create debug_mode_mm.sh
    Add script to add debug parameter to Modem Manager

---
+++ debug_mode_mm.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+## modify ModemManager.service to add " --debug"
+SERVICE_FILE="/lib/systemd/system/ModemManager.service"
+STRING_DEBUG=" --debug"
+
+
+if grep -q 'debug' <<< "$CURRENT_CONFIG";then
+	echo "debug parameter already exists"
+else
+	sudo sed -i "s/\(ExecStart.*\).*/\1${STRING_DEBUG}/g" ${SERVICE_FILE}
+	restart_mm_service=true
+fi
+
+
+
+if [ "$restart_mm_service" == "true" ]
+then
+	sudo systemctl daemon-reload
+	sudo systemctl restart ModemManager
+fi
