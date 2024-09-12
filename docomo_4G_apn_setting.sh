2d4576069a97735d877b3b6d9c527f4336c002c3
Richie Roy Jayme <58279554+Queztion@users.noreply.github.com>
2024-09-12 15:20:09 +0900

    add docomo 4G and softbank 4G apn setting
    add docomo 4G and softbank 4G apn setting

---
+++ docomo_4G_apn_setting.sh
@@ -0,0 +1,3 @@
+ #! /bin/bash
+ 
+ mmcli -m any --3gpp-set-initial-eps-bearer-settings=apn=mopera.net,ip-type=IPV4V6,allowed-auth=chap,user="",password=""
---
+++ softbank_4G_apn_setting.sh
@@ -0,0 +1,3 @@
+ #! /bin/bash
+ 
+ mmcli -m any --3gpp-set-initial-eps-bearer-settings=apn=plus.4g,ip-type=IPV4V6,allowed-auth=chap,user="plus",password="4g"
