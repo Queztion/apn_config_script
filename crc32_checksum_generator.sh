67c2b7659bcc352b586914692828508a7dc5606c
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2025-06-26 19:26:56 +0900

    Update crc32_checksum_generator.sh

--- crc32_checksum_generator.sh
+++ crc32_checksum_generator.sh
@@ -1,4 +1,39 @@
 #! /bin/bash
 
-SYS_VENDOR_VALUE=$(cat /sys/class/dmi/id/sys_vendor)
-echo -n "$SYS_VENDOR_VALUE" | crc32 > sys_vendor_crc32.txt
+#!/bin/bash
+
+# Script to calculate the CRC32 checksum of 
+# /sys/class/dmi/id/sys_vendor
+# /sys/class/dmi/id/product_name
+# sys/class/dmi/id/product_sku
+
+
+#set -x # Enable debugging: Prints each command before execution
+
+OUTPUT_FILE="sys_vendor_crc32.txt"
+SYS_VENDOR=/sys/class/dmi/id/sys_vendor
+PRODUCT_NAME=/sys/class/dmi/id/product_name
+PRODUCT_SKU=/sys/class/dmi/id/product_sku
+
+
+echo "$SYS_VENDOR:" | tee $OUTPUT_FILE
+cat $SYS_VENDOR | tee -a $OUTPUT_FILE
+echo "CRC32 checksum value for SYS_VENDOR:" | tee -a $OUTPUT_FILE
+crc32 $SYS_VENDOR | tee -a $OUTPUT_FILE
+
+echo ""
+
+echo "$PRODUCT_NAME:" | tee -a $OUTPUT_FILE
+cat $PRODUCT_NAME | tee -a $OUTPUT_FILE
+echo "CRC32 checksum value for PRODUCT_NAME:" | tee -a $OUTPUT_FILE
+crc32 $PRODUCT_NAME | tee -a $OUTPUT_FILE
+
+
+echo ""
+
+echo "$PRODUCT_SKU:" | tee -a $OUTPUT_FILE
+cat $PRODUCT_SKU | tee -a $OUTPUT_FILE
+echo "CRC32 checksum value for PRODUCT_SKU:" | tee -a $OUTPUT_FILE
+crc32 $PRODUCT_SKU | tee -a $OUTPUT_FILE
+
+
