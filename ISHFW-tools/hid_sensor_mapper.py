fa3871f87f4c16ec05be534ed3433e85466d9ad1
Roi V Sage <58279554+Queztion@users.noreply.github.com>
2025-07-22 12:00:29 +0900

    Create hid_sensor_mapper.py
    Draft hid_sensor_mapper

---
+++ ISHFW-tools/hid_sensor_mapper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+# Mapping of HID usage IDs to sensor types
+sensor_map = {
+    "200001": "Sensor Collection",
+    "200073": "Accelerometer 3D",
+    "200076": "Ambient Light Sensor",
+    "200086": "Temperature Sensor",
+    "20008a": "Humidity Sensor",
+    "200083": "Magnetometer 3D",
+    "20008e": "Pressure Sensor",
+    "20008f": "Proximity Sensor",
+    "2000b1": "Gyroscope 3D",
+    "2000b4": "Gravity Sensor",
+    "2000c1": "Linear Acceleration Sensor",
+    "2000c2": "Rotation Vector Sensor",
+    "2000e1": "Custom Sensor (Vendor-defined)",
+    "INT-020b": "Vendor-specific/Internal Sensor"
+}
+
+def find_hid_sensors(base_path="/sys/devices"):
+    for root, dirs, files in os.walk(base_path):
+        for dir_name in dirs:
+            if "HID-SENSOR" in dir_name:
+                usage_id_match = re.search(r"HID-SENSOR-([0-9a-fA-F]+|INT-\d+)", dir_name)
+                if usage_id_match:
+                    usage_id = usage_id_match.group(1)
+                    sensor_type = sensor_map.get(usage_id, "Unknown Sensor Type")
+                    full_path = os.path.join(root, dir_name)
+                    print(f"Path: {full_path}")
+                    print(f"  Usage ID: {usage_id}")
+                    print(f"  Sensor Type: {sensor_type}
+")
+
+if __name__ == "__main__":
+    find_hid_sensors()
