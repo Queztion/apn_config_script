9b6ed4e18f523a9f3044e6806b3069044e8898b0
Queztion <sijuanpo@gmail.com>
2025-12-18 20:08:11 +0900

    Added additional commands for crc32_checksum_generator.sh. The rest are updated with chmod +x  Changes to be committed: 	modified:   check_apn_AT_cgdcont.sh 	modified:   crc32_checksum_generator.sh 	modified:   debug_mode_mm.sh 	modified:   disable_acpi_RP07.sh 	modified:   dmesg_clear.sh 	modified:   dmesg_to_utc.sh 	modified:   docomo_4G_apn_setting.sh 	modified:   docomo_5G_apn_setting.sh 	modified:   gedit_ModemManager_service.sh 	modified:   iij_4G_apn_setting.sh 	modified:   journalctl_clear.sh 	modified:   set_primary_sim_slot_1.sh 	modified:   set_primary_sim_slot_2.sh 	modified:   softbank_4G_apn_setting.sh 	modified:   suspend_5s.sh 	modified:   test-low-power-suspend-resume.sh 	modified:   truncate_syslogs.sh 	modified:   wwan_issue_fix.sh

--- crc32_checksum_generator.sh
+++ crc32_checksum_generator.sh
@@ -15,6 +15,7 @@ OUTPUT_FILE="sys_vendor_crc32.txt"
 SYS_VENDOR=$(cat /sys/class/dmi/id/sys_vendor)
 PRODUCT_NAME=$(cat /sys/class/dmi/id/product_name)
 PRODUCT_SKU=$(cat /sys/class/dmi/id/product_sku)
+PRODUCT_FAMILY=$(cat /sys/class/dmi/id/product_family)
 
 # Can do it as one line 
 # echo -n $(cat /sys/class/dmi/id/sys_vendor) | crc32 /dev/stdin
@@ -25,24 +26,57 @@ PRODUCT_NAME_CRC32=$(echo -n $PRODUCT_NAME | crc32 /dev/stdin)
 # echo -n $(cat /sys/class/dmi/id/product_sku) | crc32 /dev/stdin
 PRODUCT_SKU_CRC32=$(echo -n $PRODUCT_SKU | crc32 /dev/stdin)
 
+PRODUCT_FAMILY_CRC32=$(echo -n $PRODUCT_FAMILY | crc32 /dev/stdin)
 
-echo "SYS_VENDOR String Value: $SYS_VENDOR:" | tee $OUTPUT_FILE
+
+echo "SYS_VENDOR String Value: $SYS_VENDOR" | tee $OUTPUT_FILE
 echo "CRC32 checksum value for SYS_VENDOR:" | tee -a $OUTPUT_FILE
 echo $SYS_VENDOR_CRC32 | tee -a $OUTPUT_FILE
 
 echo "" | tee -a $OUTPUT_FILE
 
-echo "PRODUCT_NAME String Value: $PRODUCT_NAME:" | tee -a $OUTPUT_FILE
+echo "PRODUCT_NAME String Value: $PRODUCT_NAME" | tee -a $OUTPUT_FILE
 echo "CRC32 checksum value for PRODUCT_NAME:" | tee -a $OUTPUT_FILE
 echo $PRODUCT_NAME_CRC32 | tee -a $OUTPUT_FILE
 
 
 echo "" | tee -a $OUTPUT_FILE
 
-echo "PRODUCT_SKU String Value: $PRODUCT_SKU:" | tee -a $OUTPUT_FILE
+echo "PRODUCT_SKU String Value: $PRODUCT_SKU" | tee -a $OUTPUT_FILE
 echo "CRC32 checksum value for PRODUCT_SKU:" | tee -a $OUTPUT_FILE
 echo $PRODUCT_SKU_CRC32 | tee -a $OUTPUT_FILE
 
 echo "" | tee -a $OUTPUT_FILE
 
-echo "Your ISH file name will be: ish_lnlm_"$SYS_VENDOR_CRC32"_"$PRODUCT_NAME_CRC32"_"$PRODUCT_SKU_CRC32".bin" | tee -a $OUTPUT_FILE
+echo "Original ISH file name will be: ish_lnlm_"$SYS_VENDOR_CRC32"_"$PRODUCT_NAME_CRC32"_"$PRODUCT_SKU_CRC32".bin" | tee -a $OUTPUT_FILE
+
+echo "" | tee -a $OUTPUT_FILE
+
+echo "Upstreamed ISH file name will be: ish_lnlm_"$SYS_VENDOR_CRC32"_"$PRODUCT_SKU_CRC32".bin" | tee -a $OUTPUT_FILE
+
+echo "" | tee -a $OUTPUT_FILE
+
+
+# PRODUCT_FAMILY version
+
+echo "PRODUCT_FAMILY String Value: $PRODUCT_FAMILY" | tee -a $OUTPUT_FILE
+echo "CRC32 checksum value for PRODUCT_FAMILY:" | tee -a $OUTPUT_FILE
+echo $PRODUCT_FAMILY_CRC32 | tee -a $OUTPUT_FILE
+
+echo "" | tee -a $OUTPUT_FILE
+
+echo "Full ISH file name using PRODUCT_FAMILY will be: ish_lnlm_"$SYS_VENDOR_CRC32"_"$PRODUCT_FAMILY_CRC32"_"$PRODUCT_NAME_CRC32"_"$PRODUCT_SKU_CRC32".bin" | tee -a $OUTPUT_FILE
+
+echo "" | tee -a $OUTPUT_FILE
+
+echo "ISH file name using PRODUCT_FAMILY will be: ish_lnlm_"$SYS_VENDOR_CRC32"_"$PRODUCT_FAMILY_CRC32".bin" | tee -a $OUTPUT_FILE
+
+echo "" | tee -a $OUTPUT_FILE
+
+
+echo "Please copy this bin file to this directory:  /lib/firmware/intel/ish" | tee -a $OUTPUT_FILE
+
+echo "" | tee -a $OUTPUT_FILE
+
+echo "Also don't forget to run after copying ISH file:    sudo update-initramfs -u -k all" | tee -a $OUTPUT_FILE
+
